4bf62e0c7fc3b8fe97723cf79e4695a856ba97c2
rhargreaves <robert@roberthargreaves.net>
2019-04-23 22:55:55 +0100

    Refactor

--- src/main.py
+++ src/main.py
@@ -7,17 +7,21 @@ parser.add_argument('file')
 args = parser.parse_args()
 
 
+def read_byte(file):
+    return file.read(1)[0]
+
+
 def main(args):
     with open(args.file, "rb") as f:
-        version = f.read(1)[0]
+        version = read_byte(f)
         print("Version {}".format(version))
 
         if version == 8:
-            instrument_mode = f.read(1)[0]
+            instrument_mode = read_byte(f)
             system_type = None
         elif version == 11:
-            system_type = f.read(1)[0]
-            instrument_mode = f.read(1)[0]
+            system_type = read_byte(f)
+            instrument_mode = read_byte(f)
 
     if instrument_mode == 1:
         print("FM")
