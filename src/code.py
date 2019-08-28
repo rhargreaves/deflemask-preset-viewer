5b5c0e83c62e33f493cc1d5799b91c215a36193d
rhargreaves <robert@roberthargreaves.net>
2019-08-28 23:30:04 +0100

    Refactor

--- src/code.py
+++ src/code.py
@@ -5,21 +5,21 @@ def const_name(filename):
     return os.path.splitext(os.path.basename(filename))[0].upper().replace(' ', '_')
 
 
-def print_code(dmp):
+def print_code(preset):
     STEREO_L_R = 3
 
     code = "static const Channel {name} = {{ {alg}, {fb}, {stereo}, {ams}, {fms}, {octave}, {freq}, {{ ".format(
-        alg=dmp.algorithm,
-        fb=dmp.feedback,
+        alg=preset.algorithm,
+        fb=preset.feedback,
         stereo=STEREO_L_R,
-        ams=dmp.lfo_ams,
-        fms=dmp.lfo_fms,
+        ams=preset.lfo_ams,
+        fms=preset.lfo_fms,
         octave=0,
         freq=0,
-        name=const_name(dmp.name)
+        name=const_name(preset.name)
     )
     for i in [0, 2, 1, 3]:
-        op = dmp.operators[i]
+        op = preset.operators[i]
         opDef = "{{ {mul}, {detune}, {ar}, {rs}, {dr}, {am}, {sa}, {d2r}, {rr}, {tl}, {ssg} }}".format(
             mul=op.mul,
             detune=op.dt,
