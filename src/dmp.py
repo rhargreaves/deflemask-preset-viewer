ef11303c6d92a9bdee6d52495aab058f683d6832
rhargreaves <robert@roberthargreaves.net>
2019-08-28 23:24:02 +0100

    Extract superclass

--- src/dmp.py
+++ src/dmp.py
@@ -1,12 +1,10 @@
+from .preset import Preset
 
-class Dmp:
+
+class Dmp(Preset):
     def __init__(self):
+        Preset.__init__(self)
         self.filename = None
         self.version = None
         self.system_type = None
         self.instrument_mode = None
-        self.lfo_fms = None
-        self.lfo_ams = None
-        self.feedback = None
-        self.algorithm = None
-        self.operators = []
---
+++ src/preset.py
@@ -0,0 +1,8 @@
+
+class Preset:
+    def __init__(self):
+        self.lfo_fms = None
+        self.lfo_ams = None
+        self.feedback = None
+        self.algorithm = None
+        self.operators = []
