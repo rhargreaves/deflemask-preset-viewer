4caaedc30347701af8b4b6f714efbaae107fc60c
rhargreaves <robert@roberthargreaves.net>
2019-08-31 21:37:50 +0100

    Print global LFO info

--- deflemask_preset_viewer/tests/unit/test_wopn.py
+++ deflemask_preset_viewer/tests/unit/test_wopn.py
@@ -4,6 +4,8 @@ import pytest
 
 def test_returns_text_info():
     wopn = Wopn()
+    wopn.lfo_enable = True
+    wopn.lfo_freq = 1
     wopn.m_banks = [WopnBank('bank0', 0), WopnBank('bank1', 1)]
     wopn.p_banks = [WopnBank('bank0', 0), WopnBank(
         'bank1', 1), WopnBank('bank2', 2)]
@@ -11,8 +13,10 @@ def test_returns_text_info():
     info = wopn.info()
 
     assert 'WOPN' in info
-    assert 'M_Banks   2' in info
-    assert 'P_Banks   3' in info
+    assert 'M_Banks    2' in info
+    assert 'P_Banks    3' in info
+    assert 'LFO       On' in info
+    assert 'LFO Freq   1' in info
 
 
 @pytest.mark.skip(reason="WIP")
--- deflemask_preset_viewer/wopn/wopn.py
+++ deflemask_preset_viewer/wopn/wopn.py
@@ -12,8 +12,11 @@ class Wopn():
 
     def info(self):
         text = 'WOPN\n'
-        text = text + 'M_Banks' + "{0:>4}\n".format(len(self.m_banks))
-        text = text + 'P_Banks' + "{0:>4}\n".format(len(self.p_banks))
+        text = text + 'M_Banks' + "{0:>5}\n".format(len(self.m_banks))
+        text = text + 'P_Banks' + "{0:>5}\n".format(len(self.p_banks))
+        text = text + 'LFO' + \
+            "{0:>9}\n".format('On' if self.lfo_enable else 'Off')
+        text = text + 'LFO Freq' + "{0:>4}\n".format(self.lfo_freq)
         return text
 
 
