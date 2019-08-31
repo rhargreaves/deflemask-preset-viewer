c6baa72f01021f882bed69cfc912fe30a9424ce4
rhargreaves <robert@roberthargreaves.net>
2019-08-31 22:03:07 +0100

    Refactor tests

--- deflemask_preset_viewer/tests/unit/test_wopn.py
+++ deflemask_preset_viewer/tests/unit/test_wopn.py
@@ -1,20 +1,33 @@
 from ...wopn.wopn import Wopn, WopnBank
 import pytest
 
+wopn = Wopn()
 
-def test_returns_text_info():
-    wopn = Wopn()
+
+def setup_module(module):
     wopn.lfo_enable = True
     wopn.lfo_freq = 1
     wopn.m_banks = [WopnBank('bank0', 0), WopnBank('bank1', 1)]
     wopn.p_banks = [WopnBank('bank0', 0), WopnBank(
         'bank1', 1), WopnBank('bank2', 2)]
 
+
+def test_returns_wopn_version_info():
     info = wopn.info()
 
     assert 'WOPN' in info
+
+
+def test_returns_bank_count_info():
+    info = wopn.info()
+
     assert 'M_Banks    2' in info
     assert 'P_Banks    3' in info
+
+
+def test_returns_global_lfo_info():
+    info = wopn.info()
+
     assert 'LFO       On' in info
     assert 'LFO Freq   1' in info
 
