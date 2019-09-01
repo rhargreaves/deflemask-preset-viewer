003c30696f80abe41be6d316d14f2041f6dbb586
rhargreaves <robert@roberthargreaves.net>
2019-09-01 01:56:34 +0100

    Refactor

--- deflemask_preset_viewer/wopn/wopn_parser.py
+++ deflemask_preset_viewer/wopn/wopn_parser.py
@@ -13,10 +13,10 @@ def parse_wopn(filename):
                 f.read(2), byteorder='little', signed=False)
         else:
             p.version = 1
-        unpacked = unpack('u16u16', f.read(4))
+        unpacked = unpack('u16u16u8', f.read(5))
         p.m_bank_count = unpacked[0]
         p.p_bank_count = unpacked[1]
-        global_lfo_reg = int.from_bytes(f.read(1), byteorder='big')
+        global_lfo_reg = unpacked[2]
         p.lfo_enable = bool(global_lfo_reg >> 3)
         p.lfo_freq = (global_lfo_reg & 0x7)
         if p.version >= 2:
